eca3ffafdbb4360387a5f3c374b9c186fd05d6a2
wyttnytt <105594685+wyttnytt@users.noreply.github.com>
2024-10-28 22:54:01 +0800

    fine

--- 3d/scripts/player.gd
+++ 3d/scripts/player.gd
@@ -2,14 +2,27 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 8
+const SENSITIVITY = 0.005
 
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+@onready var target = $camera_control/camera_target
+@onready var camera = $camera_control/camera_target/Camera3D
+@onready var control = $camera_control
+@onready var player = $"."
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		control.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		player.rotate_y(-event.relative.x * SENSITIVITY)
+		
 func _physics_process(delta: float) -> void:
-	
-	# Make camera control match player position
-	position.z
-	
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -21,12 +34,22 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	#new vector 3 direction, 	using camera rotation
+	var direction = ($camera_control.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	#rotate mesh according to direction of movement
+	
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		
 	move_and_slide()
+	
+		
+	# Make camera control match player position
+	$camera_control.position = lerp($camera_control.position, position,  0.10)
